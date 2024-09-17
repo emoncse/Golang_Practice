6de161d6fbeb5f864600ae855a30600cee3e5b09
Imran N. Emon <emon.genius@gmail.com>
2024-09-17 18:42:12 +0600

    CRUD Application with GoLang

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,10 +1,29 @@
 package controllers
 
 import (
-	"first_project/utils"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func HomeController(w http.ResponseWriter, r *http.Request) {
-	utils.JSONResponse(w, http.StatusOK, "Hello this is first test")
+// UserController handles both GET and POST requests
+func UserController(c *gin.Context) {
+	if c.Request.Method == http.MethodPost {
+		// Handle creating user
+		CreateUserController(c)
+	} else if c.Request.Method == http.MethodGet {
+		// Handle getting users
+		idParam := c.Param("id")
+
+		if idParam != "" {
+			GetUser(c)
+		} else {
+			GetUsers(c)
+		}
+	} else if c.Request.Method == http.MethodDelete {
+		DeleteUser(c)
+	} else {
+		// Return 405 if the method is not allowed
+		c.JSON(http.StatusMethodNotAllowed, gin.H{"error": "Method not allowed"})
+	}
 }
